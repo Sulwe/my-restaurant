e6e7c158b5ca44b9cb52de38a7c64ad903e0798c
Sulwe <cypriansulwe.cs@gmail.com>
2025-02-17 23:35:10 +0300

    create restaurant website navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import createNavbar from './createNavbar';
 import './styles/style.css';
-import rest from './assets/rest.jpg';
 
-const bgImg = document.getElementById('rest');
-bgImg.src = rest;
 
 createNavbar();
+
 
 
